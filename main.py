c8e6dee851ecd04d292b1c697ca797436232cfe1
Ramadhany Kurnia Pratama <ramadhanykurniapratama@gmail.com>
2026-04-14 09:34:33 +0700

    Add FastAPI entry point and update requirements for CD

---
+++ main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+from etl_process import run_pipeline
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return {"message": "Data Pipeline API is Running"}
+
+@app.get("/run-etl")
+def trigger_etl():
+    try:
+        # Jalankan fungsi ETL yang sudah kita buat sebelumnya
+        run_pipeline('data_mentah.csv', 'hasil_output.csv')
+        return {"status": "success", "message": "ETL Process Completed"}
+    except Exception as e:
+        return {"status": "error", "message": str(e)}
